af00015521166e99f09aee352f500f5b5eb3a3cd
Michael Perry <lmperry@stanford.edu>
2018-08-31 21:10:44 -0700

    remove helper scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Builds the container.
-# The container can be exported using the export.sh script
-GEAR=scitran/qa-report-fmri:v0.1.7
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build --no-cache --tag $GEAR $DIR
